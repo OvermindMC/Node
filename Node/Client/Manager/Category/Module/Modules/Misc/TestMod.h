bba52b0f65663712e32662ccd540cd889539db54
OvermindMC <Overmind@mail.com>
2023-01-21 13:29:34 +0000

    Update TestMod.h

--- Node/Client/Manager/Category/Module/Modules/Misc/TestMod.h
+++ Node/Client/Manager/Category/Module/Modules/Misc/TestMod.h
@@ -3,5 +3,27 @@
 
 class TestMod : public Module {
 public:
-	TestMod(Manager* mgr) : Module(mgr->categories[CategoryType::MISC], "TestMod") {};
+	TestMod(Manager* mgr) : Module(mgr->categories[CategoryType::MISC], "TestMod") {
+
+		registerEvent<ModuleEvent>([&](ModuleEvent* args) {
+
+			if (!args->isTicking && args->isEnabled) {
+
+				auto instance = MC::getClientInstance();
+				auto player = (instance != nullptr ? instance->getPlayer() : nullptr);
+
+				if (player) {
+
+					std::ostringstream o;
+					o << std::hex << player;
+#
+					Utils::debugOutput("Player: " + o.str());
+
+				};
+
+			};
+
+		});
+
+	};
 };
