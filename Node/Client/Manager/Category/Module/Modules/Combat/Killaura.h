1536230e71f349fb3064b2052ab91d28f3f070c1
OvermindMC <Overmind@mail.com>
2023-01-21 13:29:31 +0000

    Update Killaura.h

--- Node/Client/Manager/Category/Module/Modules/Combat/Killaura.h
+++ Node/Client/Manager/Category/Module/Modules/Combat/Killaura.h
@@ -6,13 +6,27 @@ public:
 	float range = 12.f;
 	bool multi = true;
 public:
+	size_t renderFrame = 0;
+public:
 	Killaura(Manager* mgr) : Module(mgr->categories[CategoryType::COMBAT], "Killaura") {
 
-		registerEvent<GameModeTickEvent>([&](GameModeTickEvent* args) {
+		registerEvent<RenderCtxEvent>([&](RenderCtxEvent* args) {
 
-			auto gm = args->GM;
-			auto player = gm->player;
-			auto myPos = player->getPos();
+			this->renderFrame++;
+			
+			if(this->renderFrame >= 3)
+				this->renderFrame = 0;
+			else
+				return;
+
+			auto instance = args->ctx->instance;
+			
+			auto player = (instance != nullptr ? instance->getPlayer() : nullptr);
+			auto myPos = (player != nullptr ? *player->getPos() : Vec3<float>());
+			auto gm = (player != nullptr ? player->getGameMode() : nullptr);
+
+			if(player == nullptr || gm == nullptr)
+				return;
 
 			auto I = 0;
 			forEachEntity([&](Actor* entity, bool isRegular) {
@@ -20,7 +34,7 @@ public:
 				if(I > (multi ? 1 : 0) || !isRegular || entity->runtimeId == player->runtimeId || !entity->isAlive())
 					return;
 				
-				if(entity->getPos()->dist(*myPos) > this->range)
+				if(entity->getPos()->dist(myPos) > this->range)
 					return;
 
 				I++;
@@ -29,7 +43,7 @@ public:
 
 			},
 				[&](Actor* first, Actor* second) {
-					return first->getPos()->dist(*myPos) < second->getPos()->dist(*myPos);
+					return first->getPos()->dist(myPos) < second->getPos()->dist(myPos);
 				}
 			);
 
