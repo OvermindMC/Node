5a0a0e6041e52fc262e0bd9e989de258991b76ba
OvermindMC <Overmind@mail.com>
2023-01-21 13:11:06 +0000

    Update Manager.cpp

--- Node/Client/Manager/Manager.cpp
+++ Node/Client/Manager/Manager.cpp
@@ -12,6 +12,7 @@
 /* Hooks */
 
 #include "Hook/Hooks/RenderContext/HookRenderCtx.h"
+#include "Hook/Hooks/GameMode/HookGameMode.h"
 #include "Hook/Hooks/Network/HookNetwork.h"
 #include "Hook/Hooks/Entity/HookEntity.h"
 #include "Hook/Hooks/Key/HookKey.h"
@@ -43,44 +44,10 @@ Manager::Manager(Client* c) {
 	MH_Initialize();
 
 	new HookRenderCtx(this);
+	new HookGameMode(this);
 	new HookNetwork(this);
 	new HookEntity(this);
 	new HookKey(this);
-
-	{ /* GameMode */
-
-		auto sig = Mem::findSig("48 8D 05 ? ? ? ? 48 8B D9 48 89 01 48 8B 89 ? ? ? ? 48 85 C9 74 11 48 8B 01 BA ? ? ? ? 48 8B 00 FF 15 ? ? ? ? 48 8B 8B ? ? ? ? 48 85 C9 74 17");
-
-		if (sig) {
-
-			auto offset = *(int*)(sig + 3);
-			auto VTable = (uintptr_t**)(sig + offset + 7);
-
-			new Hook<void, GameMode*>(this, "GameModeTick", (uintptr_t)VTable[8], [&](GameMode* GM) {
-
-				auto _this = getHook<void, GameMode*>("GameModeTick");
-
-				if(_this) {
-
-					for (auto [type, category] : this->categories) {
-
-						for (auto mod : category->modules) {
-
-							if (mod->isEnabled)
-								mod->callEvent<GameModeTickEvent>(GameModeTickEvent{ GM });
-
-						};
-					};
-
-					_this->_Func(GM);
-
-				};
-
-			});
-
-		};
-
-	};
 
 	for (;;) {
 
