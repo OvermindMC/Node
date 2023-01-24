591ce078a067066c528264bd5a44c51430693418
OvermindMC <Overmind@mail.com>
2023-01-24 22:13:10 +0000

    Update Minecraft.h

--- Node/Client/SDK/Minecraft.h
+++ Node/Client/SDK/Minecraft.h
@@ -5,4 +5,6 @@ class MC {
 public:
 	static auto getClientInstance(void) -> ClientInstance*;
 	static auto getLocalPlayer(void) -> Player*;
+public:
+	static auto gameIsFullScreen(void) -> bool;
 };
