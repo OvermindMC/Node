6f496f1fc9d998030154963adf08605adcc1e268
OvermindMC <Overmind@mail.com>
2023-01-31 19:20:09 +0000

    Update Player.h

--- Node/Client/SDK/Classes/Player.h
+++ Node/Client/SDK/Classes/Player.h
@@ -18,4 +18,59 @@ public:
 	virtual auto spawnAnim(void) -> void;
 public:
 	virtual auto setSprinting(bool) -> void;
+public:
+	virtual auto getHurtSound(void) -> enum LevelSoundEvent;
+	virtual auto getDeathSound(void) -> enum LevelSoundEvent;
+public:
+	virtual auto getSpeed(void) -> float;
+	virtual auto setSpeed(float) -> void;
+public:
+	virtual auto getPickRadius(void) -> float;
+public:
+	virtual auto hurtEffects(class ActorDamageSource*, float, bool, bool) -> void;
+public:
+	virtual auto getMeleeWeaponDamageBonus(class Mob*) -> float;
+	virtual auto etMeleeKnockbackBonus(void) -> __int64;
+public:
+	virtual auto travel(float, float, float) -> void;
+	virtual auto applyFinalFriction(float, bool) -> void;
+public:
+	virtual auto aiStep(class IMobMovementProxy*) -> void;
+	virtual auto aiStep(void) -> void;
+public:
+	virtual auto pushActors(void) -> void;
+public:
+	virtual auto lookAt(Actor*, float, float) -> void;
+	virtual auto isLookingAtAnEntity(void) -> bool;
+public:
+	virtual auto checkSpawnRules(bool) -> bool;
+	virtual auto checkSpawnObstruction(void) -> bool;
+public:
+	virtual auto getAttackAnim(float) -> float;
+	virtual auto getItemUseDuration(void) -> __int64;
+	virtual auto getItemUseStartupProgress(void) -> float;
+	virtual auto getItemUseIntervalProgress(void) -> float;
+	virtual auto getItemUseIntervalAxis(void) -> __int64;
+	virtual auto getTimeAlongSwing(void) -> __int64;
+public:
+	virtual auto ate(void) -> void;
+public:
+	virtual auto getMaxHeadXRot(void) -> float;
+public:
+	virtual auto isAlliedTo(class Mob*) -> bool;
+public:
+	virtual auto doHurtTarget(class Actor*, enum ActorDamageCause*) -> bool;
+public:
+	virtual auto canBeControlledByPassenger(void) -> bool;
+public:
+	virtual auto leaveCaravan(void) -> void;
+	virtual auto joinCaravan(class Mob*) -> void;
+	virtual auto hasCaravanTail(void) -> bool;
+	virtual auto getCaravanHead(void) -> class Mob*;
+public:
+	virtual auto getArmorValue(void) -> __int64;
+	virtual auto getArmorCoverPercentage(void) -> float;
+	virtual auto getToughnessValue(void) -> __int64;
+public:
+	virtual auto hurtArmorSlots(class ActorDamageSource const&, int, std::bitset<4>) -> void;
 };
