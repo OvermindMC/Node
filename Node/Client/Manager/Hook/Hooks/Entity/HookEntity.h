b880e0bf7b15ced521c1596cc9bedfb2df5775d9
OvermindMC <Overmind@mail.com>
2023-02-12 04:53:11 +0000

    Update HookEntity.h

--- Node/Client/Manager/Hook/Hooks/Entity/HookEntity.h
+++ Node/Client/Manager/Hook/Hooks/Entity/HookEntity.h
@@ -4,10 +4,10 @@
 
 class HookEntity : public Hook<void, Actor*> {
 public:
-	HookEntity(Manager* mgr) : Hook<void, Actor*>(mgr, "ActorBaseTick", Mem::findSig("48 8D 05 ? ? ? ? 48 89 03 48 C7 83 ? ? ? ? ? ? ? ? 48 8B 8B"), 51,
+	HookEntity(Manager* mgr) : Hook<void, Actor*>(mgr, "ActorNormalTick", Mem::findSig("48 8D 05 ? ? ? ? 48 89 03 48 C7 83 ? ? ? ? ? ? ? ? 48 8B 8B"), 50,
 		[&](Actor* entity) {
 			
-			auto _this = this->manager->getHook<void, Actor*>("ActorBaseTick");
+			auto _this = this->manager->getHook<void, Actor*>("ActorNormalTick");
 				
 			if(_this) {
 
